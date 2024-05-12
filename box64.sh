c62d62c8a35a893253684bc23a9745c73579a4c0
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 15:01:54 +0200

    Update and rename box64 to box64.sh

---
+++ box64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt install gpg wget curl
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+apt-get install -y ca-certificates
+cp local-ca.crt /usr/local/share/ca-certificates
+update-ca-certificates
+apt update -y
+apt install box64-android -y
+rm -rf box64.sh
