6544e6dab43ace51a0bf5651eda0e211686eb503
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 18:43:53 +0200

    Aktualisieren von ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,5 +2,5 @@
 pkg update -y && pkg upgrade -y && pkg install proot proot-distro -y && pd install ubuntu-lts
 echo 'cd ~ && pd login ubuntu-lts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
-cp Bedrock-Server/box64.sh ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/root
-echo 'box64 ~/Minecraft/bedrock_server' > ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/bin/minecraft
+cp Bedrock-Server/box64.sh ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/root && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/root/box64.sh
+echo 'box64 ~/Minecraft/bedrock_server' > ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/bin/minecraft && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/bin/minecraft
