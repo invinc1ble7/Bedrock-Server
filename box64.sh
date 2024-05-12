14ed2eecc5b007e09cc14d92d1aa3cce9e18003a
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 17:33:23 +0200

    Aktualisieren von box64.sh

--- box64.sh
+++ box64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt upgrade && apt install git gpg wget curl
+apt update -y && apt upgrade -y && apt install git gpg wget curl -y
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
 wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
 apt update -y && apt install box64-android -y
