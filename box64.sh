b71cb65c3709f9154acc79f96486ef96ac25baea
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 16:42:17 +0200

    Update box64.sh

--- box64.sh
+++ box64.sh
@@ -2,5 +2,4 @@
 apt update && apt upgrade && apt install git gpg wget curl
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
 wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
-apt-get install -y ca-certificates && cp local-ca.crt /usr/local/share/ca-certificates && update-ca-certificates
 apt update -y && apt install box64-android -y
