08b60047d44c68ffd7217c4b579a391a48c5d71c
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 15:55:03 +0200

    Aktualisieren von box64.sh

--- box64.sh
+++ box64.sh
@@ -5,7 +5,7 @@ proot-distro login ubuntu-lts -- apt install git gpg wget curl
 
 proot-distro login ubuntu-lts -- wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
 
-proot-distro login ubuntu-lts -- wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+proot-distro login ubuntu-lts -- wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | proot-distro login ubuntu-lts -- gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
 
 proot-distro login ubuntu-lts -- apt-get install -y ca-certificates && cp local-ca.crt /usr/local/share/ca-certificates && update-ca-certificates
 
