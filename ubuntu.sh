5547e48f50e1c3f1076a913b95a753c60fbfd2d1
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-12 14:00:58 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 pkg update -y && pkg upgrade && pkg install proot proot-distro && pd install ubuntu-lts
-cd ../usr/bin && nano ubuntu && chmod +x ubuntu
 echo 'cd ~ && pd login ubuntu-lts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
