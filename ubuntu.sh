036ba104d3eade5b257fc6fedd885ac31ca6bb11
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-21 02:19:02 +0200

    Aktualisieren von ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pkg update -y && pkg upgrade -y && pkg install proot proot-distro -y && pd install ubuntu-lts
-echo 'cd ~ && pd login ubuntu-lts' > ../usr/bin/ubuntu
+pkg update -y && pkg upgrade -y && pkg install proot proot-distro -y && pd install ubuntu
+echo 'cd ~ && pd login ubuntu' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
-cp Bedrock-Server/box64.sh ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/root && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/root/box64.sh
-proot-distro login ubuntu-lts -- ./box64.sh
-echo 'cd ~/Minecraft && box64 bedrock_server' > ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/bin/minecraft && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu-lts/bin/minecraft
+cp Bedrock-Server/box64.sh ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/root && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/box64.sh
+proot-distro login ubuntu -- ./box64.sh
+echo 'cd ~/Minecraft && box64 bedrock_server' > ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/bin/minecraft && chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/bin/minecraft
 cd ~ && rm -rf Bedrock-Server
